f672cfea1b25e4bde3ca7937330b6def9bb3dc22
udaykirankota32 <kotaudaykiran239@gmail.com>
2026-06-29 11:41:59 +0530

    fix:files names mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useMemo } from "react";
 import useUsers from "./hooks/useUsers";
-import { SORT_ORDER, SORT_FIELDS } from "./utils/Constants";
+import { SORT_ORDER, SORT_FIELDS } from "./utils/constants";
 
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
